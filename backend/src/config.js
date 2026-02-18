972a9b3de7ab07d2b436c40395dd808eefb9cf09
Danny Ryan <dannykryan@gmail.com>
2026-02-18 20:03:46 +0000

    remove unused config file

--- backend/src/config.js
+++
@@ -1,6 +0,0 @@
-export const config = {
-  JWT_SECRET: process.env.JWT_SECRET || "SECRET_KEY",
-  DATABASE_URL: process.env.DATABASE_URL,
-  DIRECT_URL: process.env.DIRECT_URL,
-  PORT: process.env.PORT || 3000,
-};
